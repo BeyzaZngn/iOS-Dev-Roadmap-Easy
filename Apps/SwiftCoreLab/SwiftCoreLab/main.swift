18ef0cb542a1b76023320045e38225d91f3b85de
Beyza Zengin <112340720+BeyzaZngn@users.noreply.github.com>
2026-01-11 17:16:25 +0300

    D0 - repo + projects

---
+++ Apps/SwiftCoreLab/SwiftCoreLab/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftCoreLab
+//
+//  Created by Beyza Zengin on 11.01.2026.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Apps/SwiftUILab/SwiftUILab/SwiftUILabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILabApp.swift
+//  SwiftUILab
+//
+//  Created by Beyza Zengin on 11.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
